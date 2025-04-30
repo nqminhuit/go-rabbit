e098b8c22ee832e6688e74a3e5dba81441987cf5
minh <minh.q.nguyen@opswat.com>
2025-04-30 17:42:17 +0700

    tmp

--- receiver/receive.go
+++ receiver/receive.go
@@ -10,10 +10,63 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func sendDataToOpenSearch() {
-	// listen to a buffer channel
-	// when the channel reach max capacity
-	// send data to opensearch
+func compactJson(msgs <-chan amqp.Delivery) <-chan *bytes.Buffer {
+	out := make(chan *bytes.Buffer)
+	go func() {
+		for msg := range msgs {
+			compacted := &bytes.Buffer{}
+			err := json.Compact(compacted, msg.Body)
+			utils.LogOnError(err, "Could not process json")
+
+			err = msg.Ack(false)
+			utils.LogOnError(err, "Could not ack message")
+			out <- compacted
+		}
+		close(out)
+	}()
+	return out
+}
+
+func addMetadata(msgs <-chan *bytes.Buffer) <-chan map[string]any {
+	out := make(chan map[string]any)
+	go func() {
+		var data map[string]any
+		for msg := range msgs {
+			err := json.Unmarshal(msg.Bytes(), &data)
+			utils.LogOnError(err, "Could not unmarshal json")
+			out <- data
+		}
+		close(out)
+	}()
+	return out
+}
+
+func batch(data <-chan map[string]any, batchSize int) <-chan []map[string]any {
+	out := make(chan []map[string]any)
+	go func() {
+		batched := make([]map[string]any, 0, batchSize)
+		for msg := range data {
+			if len(batched) < batchSize {
+				batched = append(batched, msg)
+			} else {
+				out <- batched
+				slog.Info("batch", "batched", batched)
+				batched = append(make([]map[string]any, 0, batchSize), msg)
+			}
+		}
+		out <- batched
+		close(out)
+	}()
+	return out
+}
+
+func sendDataToOpenSearch(data <-chan []map[string]any) {
+	go func() {
+		for msg := range data {
+			_ = msg
+			// slog.Info("sendDataToOpenSearch", "msg", msg)
+		}
+	}()
 }
 
 func receive(ch *amqp.Channel, q amqp.Queue) {
@@ -27,34 +80,10 @@ func receive(ch *amqp.Channel, q amqp.Queue) {
 		nil)
 	utils.FailOnError(err, "Failed to register a consumer")
 
-	go func() {
-		i := 0
-		for d := range msgs {
-			// 1. compact json
-			compacted := &bytes.Buffer{}
-			err := json.Compact(compacted, d.Body)
-			utils.LogOnError(err, "Could not process json")
-
-			// 2. add metadata to json:
-			// "deploymentId", deploymentId,
-			// "accountId", accountId,
-			// "instanceId", instanceId,
-			// "instanceName", SecurityContextUtils.getInstanceName(),
-			// "groupId", groupId,
-			// "groupName", SecurityContextUtils.getGroupName(),
-			// "retentionMs", retentionMs
-
-			// 3. send to opensearch
-			// make a buffer channel with size = 50
-			// send json to that buffer channel
-			// ack
-
-			slog.Info("Message processed", "i", i)
-			err = d.Ack(false)
-			utils.LogOnError(err, "Could not ack message")
-			i++
-		}
-	}()
+	compactJson := compactJson(msgs)
+	addedMetadata := addMetadata(compactJson)
+	batched := batch(addedMetadata, 5)
+	sendDataToOpenSearch(batched)
 
 	slog.Info("Waiting for messages, to exit press ^C")
 
